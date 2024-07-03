2c44c288a70e64e83d053af898ba50c7816e2783
nyanyacyan <nyanyacyan@gmail.com>
2024-07-03 10:47:18 +0900

    ログを消去して毎回わかりやすいように修正。タイムゾーンも東京に修正

--- installer/src/process.sh
+++ installer/src/process.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
+# ログファイルの消去
+> /var/log/my_script.log
+
 # ログファイルの設定
 LOG_FILE="/var/log/my_script.log"
 exec > >(tee -a $LOG_FILE) 2>&1
 
 # 仮想環境のアクティブ化
+# shellcheck disable=SC1091
 source /home/ec2-user/venv/bin/activate
 
 # スクリプトの開始
-echo "$(date) - Starting script"
-cd /home/ec2-user/site_monitor/installer/src || { echo "$(date) - Failed to change directory"; exit 1; }
-python main.py 2>&1 | tee -a /var/log/my_script.log
+{
+    # 日本時間に修正
+    export TZ=Asia/Tokyo
 
-# /tmp ディレクトリのクリーンアップ
-echo "$(date) - Cleaning up /tmp directory..."
-find /tmp -type f -atime +10 -exec rm -f {} \;
-find /tmp -type d -empty -atime +10 -exec rm -rf {} \;
-echo "$(date) - Cleanup completed."
+    echo "$(date) - Starting script"
+    cd /home/ec2-user/site_monitor/installer/src || { echo "$(date) - Failed to change directory"; exit 1; }
+    # shellcheck disable=SC2094
+    python main.py
 
-# 仮想環境のディアクティベート
-deactivate
+    # /tmp ディレクトリのクリーンアップ
+    echo "$(date) - Cleaning up /tmp directory..."
+    find /tmp -type f -atime +10 -exec rm -f {} \;
+    find /tmp -type d -empty -atime +10 -exec rm -rf {} \;
+    echo "$(date) - Cleanup completed."
 
+    # 仮想環境のディアクティベート
+    deactivate
 
-echo "$(date) - Cleaning up /tmp directory..."
-find /tmp -type f -atime +10 -exec rm -f {} \;
-find /tmp -type d -empty -atime +10 -exec rm -rf {} \;
-echo "$(date) - Cleanup completed."
+    # インスタンスを停止
+    echo "$(date) - Stopping instance"
+    sleep 10
+    aws ec2 stop-instances --instance-ids i-0a1629d8026f15e8c
 
-# インスタンスを停止
-echo "$(date) - Stopping instance"
-sleep 10
-aws ec2 stop-instances --instance-ids i-0a1629d8026f15e8c
-
-echo "$(date) - Script finished"
+    echo "$(date) - Script finished"
+}  | tee -a $LOG_FILE
